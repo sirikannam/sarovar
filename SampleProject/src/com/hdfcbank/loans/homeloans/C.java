8622cf077fff3f431f7612c9eb1bd7a686789da7
sirikannam <siri.kannam1@gmail.com>
2018-12-13 19:43:53 -0500

    SampleProject is added
    SampleProject is added

---
+++ SampleProject/src/com/hdfcbank/loans/homeloans/C.java
@@ -0,0 +1,22 @@
+package com.hdfcbank.loans.homeloans;
+
+import com.hdfcbank.loans.carloans.A;
+
+public class C extends A
+{
+   void m3()
+   {
+	   System.out.println("iam m3 in class C");
+	   System.out.println("Hello world");
+   }
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C c=new C();
+		c.m3();
+		c.m1();
+		
+
+	}
+
+}
